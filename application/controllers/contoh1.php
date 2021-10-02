11c14a6ed97a1a533403222bc8680b593df5bfef
Jayantiadnela <jayantiadnela10@gmail.com>
2021-10-02 13:22:35 +0700

    upload project baru

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Jayanti Adnela 
+              Saya tinggal di daerah pontianak
+              Saya sangat suka menonton film";
+    }
+} 
---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Jayanti</li>
+        <li>Nama Belakang : Adnela</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Parit Pangeran BTN Citra Sejahtera No. A3</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Pontianak</li>
+    </ul>
+    
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Volly</li>
+        <li>Bulu tangkis</li>
+    </ul>
+</section>
