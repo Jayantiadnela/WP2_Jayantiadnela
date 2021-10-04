968c3445a07c57dc6b163ce617bc0560547dfbcb
Jayantiadnela <jayantiadnela10@gmail.com>
2021-10-04 19:51:08 +0700

    upload project 4

---
+++ application/views/view-data-matakuliah.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>Tampil Data Matakuliah</title>
+</head>
+
+<body>
+    <center>
+        <table>
+            <tr>
+                <th colspan="3">
+                    Tampil Data Mata Kuliah
+</th>
+</tr>
+<tr>
+    <td colspan="3">
+        <hr>
+</td>
+</tr>
+<tr>
+    <th>Kode MTK</th>
+    <th>:</th>
+    <td>
+</tr>
+<tr>
+    <td>Nama MTK</td>
+    <td>:</td>
+    <td>
+        <?=$nama; ?>
+</td>
+</tr>
+</table>
+</center>
+</body>
+</html>
